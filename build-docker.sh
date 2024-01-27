4c97334ae725b219bbdeec8d32533149e5ec9303
Gold856 <117957790+Gold856@users.noreply.github.com>
2024-01-27 00:28:21 -0500

    Remove patch copy

--- build-docker.sh
+++ build-docker.sh
@@ -6,6 +6,6 @@ source versions.sh
 
 docker pull ${DOCKER_IMAGE}
 docker run -v ${PWD}:/artifacts ${DOCKER_IMAGE} bash -c "\
-    cp /artifacts/*.jinfo /artifacts/*.tar.xz /artifacts/*.sh /artifacts/*.patch /artifacts/control /artifacts/postinst /artifacts/prerm . \
+    cp /artifacts/*.jinfo /artifacts/*.tar.xz /artifacts/*.sh /artifacts/control /artifacts/postinst /artifacts/prerm . \
     && ./setup.sh \
     && ./build.sh"
